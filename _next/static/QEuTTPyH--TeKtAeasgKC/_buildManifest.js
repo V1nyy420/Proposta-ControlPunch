afccff38c4fb5c86c67f7a3040040f94c02a4b95
Gabriel Rossanesi <gabrielrossanesi@outlook.com>
2026-05-16 12:12:34 -0300

    Deploy static site to GitHub Pages

---
+++ _next/static/QEuTTPyH--TeKtAeasgKC/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Controlpunch//_next/:path+",
+        "destination": "/Controlpunch/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
